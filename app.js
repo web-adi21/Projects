8e1e802740071d79b73bebec987e230fbdfb3d21
Aditya Ojha <adityaojha2043@gmail.com>
2026-07-22 07:52:06 +0530

    adding joi for schema validations

--- app.js
+++ app.js
@@ -112,7 +112,7 @@ app.all("*", (req,res,next) => {
 
 app.use((err,req,res,next) => {
   let { statusCode = 500, message = "Something went wrong" } = err;
-  res.status(statusCode).send(message);
+  res.status(statusCode).render('error.ejs', {message});
 })
 
 app.listen(3000, () => {
