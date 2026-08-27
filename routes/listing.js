f8a667ce2a9d74fc35dd067a5d9a3932e534cf9f
Aditya Ojha <adityaojha2043@gmail.com>
2026-08-27 16:59:02 +0530

    installing multer and setting it up to upload Images and files

--- routes/listing.js
+++ routes/listing.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const wrapAsync = require('../utils/wrapAsync.js');
 const listingController = require("../controllers/listings.js");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/"});
 
 const Listing = require("../models/listing.js");
 const {isLoggedIn, isOwner, validateListing} = require("../middleware.js");
@@ -16,7 +18,11 @@ router.route("/")
     //Index route
   .get( wrapAsync(listingController.index))
     //new route
-  .post(isLoggedIn,validateListing, wrapAsync(listingController.newPost));
+  // .post(isLoggedIn,validateListing, wrapAsync(listingController.newPost));
+  .post( upload.single("listing[image]"), (req,res) => {
+    res.send(req.file);
+  })
+
 
 router.route("/:id")
   //show route
